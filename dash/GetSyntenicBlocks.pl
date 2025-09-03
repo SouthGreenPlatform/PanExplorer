45d3a8be715b5830d94c5885197ded5144ecfd62
dereeper <alexis.dereeper@ird.fr>
2025-09-03 11:13:00 +0200

    add Clinker visualisation

--- dash/GetSyntenicBlocks.pl
+++ dash/GetSyntenicBlocks.pl
@@ -5,6 +5,7 @@ use strict;
 my $gff_dir = $ARGV[0];
 my $coregene_file = $ARGV[1];
 my $output = $ARGV[2];
+my $minimal_size_blocks_to_be_displayed = $ARGV[3];
 
 my %order_by_strain;
 my %info_cluster;
@@ -146,6 +147,7 @@ foreach my $strain(keys(%order_by_strain)){
 close(ORDER_CLUSTERS2);
 
 my %final_blocks;
+my %strand_of_blocks;
 my $concat = "";
 open(ORDER_CLUSTERS3,">$output.order_clusters3.txt");
 foreach my $group(keys(%groups_of_clusters)){
@@ -165,9 +167,11 @@ foreach my $group(keys(%groups_of_clusters)){
                                 my $end_last = $sorted_list_positions_clusters[$#sorted_list_positions_clusters];
 
                                 my $strand = "-";
+                                #print "$first_cluster $last_cluster\n";
                                 if ($start_first == $info_cluster{$first_cluster}{$strain}{"start"} && $end_last == $info_cluster{$last_cluster}{$strain}{"end"}){
                                         $strand = "+";
                                 }
+                                $strand_of_blocks{$start_first}{$strain} = $strand;
                                 #my $num_track = $info_cluster{$last_cluster}{$strain}{"num_track"};
                                 $num_track++;
                                 my $taxid = $info_cluster{$last_cluster}{$strain}{"taxid"};
@@ -179,8 +183,10 @@ foreach my $group(keys(%groups_of_clusters)){
                         print ORDER_CLUSTERS3 "$group\n";
 }
 close(ORDER_CLUSTERS3);
+#print "$concat\n";
 
 my $num_block = 0;
+my %limits_of_blocks;
 open(B,"$output.order_clusters3.txt");
 open(OUT,">$output");
 print OUT ",".join(",",keys(%order_by_strain)).",num_block\n";
@@ -196,9 +202,157 @@ while(<B>){
                 foreach my $strain(keys(%order_by_strain)){
                         my $position = $info_cluster{$cluster}{$strain}{"start"};
                         print OUT ",$position";
+                        $limits_of_blocks{$strain}{$num_block} .= "$position,";
                 }
                 print OUT ",$num_block\n";
         }
 }
 close(B);
 close(OUT);
+my $max_num_block = $num_block;
+
+########################################################
+# construct JSON file for visualization with Clinker
+########################################################
+my %colinear_block_infos;
+my $json = "{\n";
+$json .= "\"clusters\":[\n";
+foreach my $strain(keys(%order_by_strain)){
+        $json .= "\t{\n";
+        $json .= "\t\"uid\":\"$strain\",\n";
+        $json .= "\t\"name\":\"$strain\",\n";
+        $json .= "\t\"loci\":[\n";
+        $json .= "\t{\n";
+
+        $json .= "\t\t\"uid\":\"$strain\",\n";
+        $json .= "\t\t\"name\":\"$strain\",\n";
+        $json .= "\t\t\"start\":0,\n";
+        $json .= "\t\t\"end\":100000,\n";
+        $json .= "\t\t\"genes\":[\n";
+        for (my $num_block = 1; $num_block <= $max_num_block; $num_block++){
+                my $positions = $limits_of_blocks{$strain}{$num_block};
+                my @pos = split(/,/,$positions);
+                my $start = $pos[0];
+                my $end = $pos[$#pos];
+                my $strand;
+                if ($strand_of_blocks{$start}{$strain}){
+                        $strand = $strand_of_blocks{$start}{$strain};
+                }
+                elsif ($strand_of_blocks{$end}{$strain}){
+                        $strand = $strand_of_blocks{$end}{$strain};
+                }
+                
+                my $strand_value = "1";
+                if ($strand eq "-"){
+                        $strand_value = "-1";
+                }
+                #print "$strain $start $end $strand\n";
+                my $number_of_genes_in_block = scalar @pos;
+                $colinear_block_infos{$num_block} = $number_of_genes_in_block;
+
+                if ($number_of_genes_in_block > $minimal_size_blocks_to_be_displayed){
+                        my $bloc_name = $strain."_block".$num_block;
+                        $start = $start / 50;
+                        $end = $end / 50;
+
+                        $json .= "\t\t{\n";
+                        $json .= "\t\t\t\"uid\":\"$bloc_name\",\n";
+                        $json .= "\t\t\t\"label\":\"$bloc_name\",\n";
+                        if ($start < $end){
+                                $json .= "\t\t\t\"start\":$start,\n";
+                                $json .= "\t\t\t\"end\":$end,\n";
+                        }
+                        else{
+                                $json .= "\t\t\t\"start\":$end,\n";
+                                $json .= "\t\t\t\"end\":$start,\n";
+                        }
+                        $json .= "\t\t\t\"strand\":$strand_value\n";
+
+                        $json .= "\t\t},\n";
+                }
+                
+        }
+        chop($json);chop($json);
+
+        $json .= "\n\t\t]\n";
+        $json .= "\t}\n";
+        $json .= "\t]\n";
+        $json .= "\t},\n";
+}
+chop($json);chop($json);
+
+$json .= "],\n";
+$json .= "\"links\":[\n";
+my $uid_link = 0;
+my %links_done;
+for (my $num_block = 1; $num_block <= $max_num_block; $num_block++){
+
+        my $number_of_genes_in_block = $colinear_block_infos{$num_block};
+        if ($number_of_genes_in_block <= $minimal_size_blocks_to_be_displayed){
+                next;
+        }
+        foreach my $strain1(keys(%order_by_strain)){
+                foreach my $strain2(keys(%order_by_strain)){
+                        if ($strain1 eq $strain2){next;}
+                        my $query = $strain1."_block".$num_block;
+                        my $target = $strain2."_block".$num_block;
+                        
+                        if (!$links_done{$target}{$query}){
+                                $uid_link++;
+                                $json .= "\t{\n";
+                                $json .= "\t\t\"uid\":\"$uid_link\",\n";
+
+                                $json .= "\t\t\"query\":{\n";
+                                $json .= "\t\t\t\"uid\":\"$query\",\n";
+                                $json .= "\t\t\t\"label\":\"$query\"\n";
+                                $json .= "\t\t},\n";
+
+                                $json .= "\t\t\"target\":{\n";
+                                $json .= "\t\t\t\"uid\":\"$target\",\n";
+                                $json .= "\t\t\t\"label\":\"$target\"\n";
+                                $json .= "\t\t},\n";
+
+                                $json .= "\t\t\"identity\":0.5,\n";
+                                $json .= "\t\t\"similarity\":0.5\n";
+
+                                $json .= "\t},\n";
+
+                                $links_done{$query}{$target} = 1;
+                                $links_done{$target}{$query} = 1;
+                        }
+                        
+                }
+        }
+}
+chop($json);chop($json);
+$json .= "\n],\n";
+
+$json .= "\"groups\":[\n";
+for (my $num_block = 1; $num_block <= $max_num_block; $num_block++){
+        my $number_of_genes_in_block = $colinear_block_infos{$num_block};
+        if ($number_of_genes_in_block <= $minimal_size_blocks_to_be_displayed){
+                next;
+        }
+
+        $json .= "\t{\n";
+        my $number_of_genes_in_block = $colinear_block_infos{$num_block};
+        $json .= "\t\t\"uid\":\"$num_block\",\n";
+        $json .= "\t\t\"label\":\"Colinear block $num_block: $number_of_genes_in_block genes\",\n";
+        $json .= "\t\t\"genes\":[\n";
+        foreach my $strain(keys(%order_by_strain)){
+                my $bloc_name = $strain."_block".$num_block;
+                $json .= "\t\t\t\"$bloc_name\",\n";
+        }
+        chop($json);chop($json);
+        $json .= "\n";
+        $json .= "\t\t]\n";
+        $json .= "\t},\n";
+}
+chop($json);chop($json);
+$json .= "\n";
+$json .= "]\n";
+$json .= "}\n";
+
+open(JSON,">$output.clinker.json");
+print JSON $json;
+close(JSON);
