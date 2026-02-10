095cee01357de2d6c663036b7747437c10c55845
dereeper <alexis.dereeper@ird.fr>
2026-02-10 17:25:01 +0100

    add citation

--- dash/homepage.py
+++ dash/homepage.py
@@ -3,6 +3,59 @@ import dash_ag_grid as dag
 
 import dash_bootstrap_components as dbc
 
+how_to_cite = html.Div(
+    style={
+        "position": "absolute",
+        "width": "92%",
+        "padding": "10px 10px 5px 10px",
+        "backgroundColor": "white",
+        "border": "1px solid lightgrey",
+        "borderRadius": "5px",
+        "marginTop": "15px",
+        "marginLeft": "10px",
+    },
+    children=[
+
+        html.Span(
+            "How to cite",
+            className="badge badge-primary how-to-cite-badge",
+            style={
+                "position": "absolute",
+                "top": "-10px",
+                "left": "-10px",
+                "backgroundColor": "#007bff",
+                "color": "white",
+                "padding": "2px 8px",
+                "borderRadius": "10px",
+                "fontSize": "0.95em",
+                "fontWeight": "600",
+                "fontFamily": "Arial, sans-serif",
+            },
+        ),
+
+        html.Span(
+            [
+                "Dereeper A, Summo M, Meyer DF. (2022) ",
+                html.B(
+                    "PanExplorer: a web-based tool for exploratory analysis and visualization of bacterial pan-genomes."
+                ),
+                " Bioinformatics. 2022 Sep 15;38(18):4412-4414. ",
+                html.A(
+                    "https://doi.org/10.1093/bioinformatics/btac504",
+                    href="https://doi.org/10.1093/bioinformatics/btac504",
+                    target="_blank",
+                ),
+            ],
+            style={
+                "fontWeight": "bold",
+                "fontSize": "1em",
+                "fontFamily": "Arial, sans-serif",
+            },
+        ),
+    ],
+)
+
+
 layout = html.Div(
     [
 
@@ -41,6 +94,11 @@ layout = html.Div(
 For general questions, comments or problems about the site or the data, please contact [alexis.dereeper@ird.fr](mailto:alexis.dereeper@ird.fr)    
 '''),
 
+        how_to_cite,
+        html.Br(), 
+        html.Br(),
+        html.Br(), 
+        html.Br(),
         html.Footer(
                     className="footer",
                     children=[
