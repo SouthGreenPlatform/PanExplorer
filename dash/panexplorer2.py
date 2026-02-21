4e5fad9f641a40ed185abd2d2881a87da0a741d6
dereeper <alexis.dereeper@ird.fr>
2026-02-21 09:32:50 +0100

    Add files via upload

--- dash/panexplorer2.py
+++ dash/panexplorer2.py
@@ -4684,6 +4684,7 @@ def heatmap_PAV(proj_title,session,specific_to,ordering,sample_ordering,metadata
             col=1
         )
         fig.update_yaxes(title_text="-log10(pvalues)", row=2, col=1)
+        fig.update_yaxes(autorange="reversed", row=1, col=1)
         fig.update_layout(height=1100)
         fig.update_layout(title_text='Presence/Absence Variation (PAV) matrix of genes across selected genomes. Pan-GWAS results are shown below the PAV matrix.')
     else:
@@ -4705,7 +4706,7 @@ def heatmap_PAV(proj_title,session,specific_to,ordering,sample_ordering,metadata
         )
                             
         fig.update_layout(title_text='Presence/Absence Variation (PAV) matrix of genes across selected genomes',xaxis_title="Gene clusters",yaxis_title="Samples")
-    
+        fig.update_yaxes(autorange="reversed")
 
 
     # fig = go.Figure(
@@ -4730,8 +4731,7 @@ def heatmap_PAV(proj_title,session,specific_to,ordering,sample_ordering,metadata
         height=1100
     )
 
-    # Important pour l'ordre naturel des lignes
-    fig.update_yaxes(autorange="reversed")
+    
 
     return fig
 
